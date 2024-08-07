a5b3b48b71782a1b90c8045a75a6eecd17351b8c
Cesar Aguayo <c.aguayo.s@outlook.com>
2024-08-07 15:05:49 -0600

    concentrado de ventas

--- src/layouts/MenuList.vue
+++ src/layouts/MenuList.vue
@@ -1,5 +1,12 @@
 <template>
   <q-list bordered padding class="rounded-borders text-primary">
+    <q-item clickable v-ripple @click="openNewTab" active-class="my-menu-link">
+      <q-item-section avatar>
+        <q-icon name="shopping_bag" />
+      </q-item-section>
+
+      <q-item-section>Historico de ventas</q-item-section>
+    </q-item>
     <q-item
       clickable
       v-ripple
@@ -95,6 +102,14 @@ import { ref } from "vue";
 import { checkRole } from "src/boot/checks";
 
 const link = ref("inbox");
+
+const openNewTab = () => {
+  // Aquí pones la URL que quieres abrir
+  const url =
+    "https://docs.google.com/spreadsheets/d/1QtsTk2NHQGDlIPl1n9GUMTd6-MZgGGFu_GposORPbyg/edit?usp=sharing";
+  // Abre la URL en una nueva pestaña
+  window.open(url, "_blank");
+};
 </script>
 
 <style lang="sass">
